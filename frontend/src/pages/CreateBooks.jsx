f549d4071500188484c9a00a77b9f3f9c237ef7f
TAUSEEF <mohdtauseef.ma32@gmail.com>
2023-11-14 21:51:53 +0530

    initial commit

---
+++ frontend/src/pages/CreateBooks.jsx
@@ -0,0 +1,79 @@
+import React, { useState } from 'react';
+import BackButton from '../components/BackButton';
+import Spinner from '../components/Spinner';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import { useSnackbar } from 'notistack';
+
+const CreateBooks = () => {
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+  const [publishYear, setPublishYear] = useState('');
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const { enqueueSnackbar } = useSnackbar();
+
+  const handleSaveBook = () => {
+    const data = {
+      title,
+      author,
+      publishYear,
+    };
+    setLoading(true);
+    axios
+      .post('http://localhost:5555/books', data)
+      .then(() => {
+        setLoading(false);
+        enqueueSnackbar('Profile Added successfully', { variant: 'success' });
+        navigate('/');
+      })
+      .catch((error) => {
+        setLoading(false);
+        // alert('An error happened. Please Check console');
+        enqueueSnackbar('Error', { variant: 'error' });
+        console.log(error);
+      });
+  };
+
+  return (
+    <div className='p-4'>
+      <BackButton />
+      <h1 className='text-3xl text-sky-300 hover:text-purple-600 my-4'>ADD PORTFOLIO</h1>
+      {loading ? <Spinner /> : ''}
+      <div className='flex flex-col border-2 border-blue-400 rounded-xl w-[600px] p-4 mx-auto'>
+        <div className='my-4'>
+          <label className='text-xl mr-4 text-sky-300 hover:text-purple-600'>STUDENT NAME</label>
+          <input
+            type='text'
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            className='border-2 border-blue-500 px-4 py-2 w-full'
+          />
+        </div>
+        <div className='my-4'>
+          <label className='text-xl mr-4 text-sky-300 hover:text-purple-600'>FIELD</label>
+          <input
+            type='text'
+            value={author}
+            onChange={(e) => setAuthor(e.target.value)}
+            className='border-2 border-blue-500 px-4 py-2  w-full '
+          />
+        </div>
+        <div className='my-4'>
+          <label className='text-xl mr-4 text-sky-300 hover:text-purple-600'>YEAR OF GRADUATION</label>
+          <input
+            type='number'
+            value={publishYear}
+            onChange={(e) => setPublishYear(e.target.value)}
+            className='border-2 border-blue-500 px-4 py-2  w-full '
+          />
+        </div>
+        <button className='p-4 bg-red-300 bg-sky-300 m-10' onClick={handleSaveBook}>
+          ADD PROFILE
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default CreateBooks
